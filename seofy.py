91d3c4e71d97591a5c3d9d5b64e937ded0877614
dartungar <dartungar@gmail.com>
2018-05-17 16:34:57 +0300

    Apps page and small navbar tweak
    - created apps page, with cards leading to actual apps
    - made a 'split button dropdown' so user can click on '/apps/' link or see dropdown

--- seofy.py
+++ seofy.py
@@ -1,10 +1,15 @@
+# import a dictionary of phrases
+# which will 'spice up' user input
+# to make it more 'SEO-like'
 def import_spice(file_path):
+	'''get dict of lines from .txt fime'''
 	spice = []
 	with open(file_path, 'r') as file:
 		for line in file:
 			spice.append(line.strip())
 	return spice
 
+# add 'spice' phrases to raw text to make it 'SEOfied'
 def seofy_text(raw_text, spice):
 	'''takes some lines of text, spices them up with SEO stuff and returns as a list'''
 	seofied_text = []
